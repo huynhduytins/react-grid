44406187d78a7e3eff82abf56a6bdf8e143407b5
Tin Huynh <tin.huynh@enouvo.com>
2025-07-09 09:14:16 +0700

    init ❤️

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { ReactGrid, type Column, type Row } from "@silevis/reactgrid";
+import "@silevis/reactgrid/styles.css";
+
+interface Person {
+  name: string;
+  surname: string;
+}
+
+const getPeople = (): Person[] => [
+  { name: "Thomas", surname: "Goldman" },
+  { name: "Susie", surname: "Quattro" },
+  { name: "", surname: "" }
+];
+
+const getColumns = (): Column[] => [
+  { columnId: "name", width: 150 },
+  { columnId: "surname", width: 150 },
+];
+
+const headerRow: Row = {
+  rowId: "header",
+  cells: [
+    { type: "header", text: "Name" },
+    { type: "header", text: "Surname" },
+  ]
+};
+
+const getRows = (people: Person[]): Row[] => [
+  headerRow,
+  ...people.map<Row>((person, idx) => ({
+    rowId: idx,
+    cells: [
+      { type: "text", text: person.name },
+      { type: "text", text: person.surname }
+    ]
+  }))
+];
+
+export default function App() {
+  const [people] = React.useState<Person[]>(getPeople());
+
+  const rows = getRows(people);
+  const columns = getColumns();
+
+  return <ReactGrid rows={rows} columns={columns} />;
+}
